185ae101efc0fb4bfa1292fb8459b076f077a79f
dalvarez59 <160683173+dalvarez59@users.noreply.github.com>
2024-02-28 23:35:00 -0700

    Create cis129_lab03_coffeeShop.py
    Coffee and Muffin Shop Simulator

---
+++ cis129_lab03_coffeeShop.py
@@ -0,0 +1,28 @@
+print("My Coffee and Muffin Shop Receipt")
+
+def calculate_total_cost(coffee_amount, muffin_amount):
+    #Prices
+    coffee_price = 5
+    muffin_price = 4
+    tax_rate = .06
+    
+    #Subtotal
+    subtotal = (coffee_amount * coffee_price) + (muffin_amount * muffin_price)
+    
+    #Taxes
+    tax_amount = subtotal * tax_rate
+    
+    #Total Cost
+    total_cost = subtotal + tax_amount
+    
+    return total_cost
+    
+#Customer's order
+coffee_amount = int(input("Amount of Coffees: "))
+muffin_amount = int(input("Amount of Muffins: "))
+
+#Calculate total cost 
+total_cost = calculate_total_cost(coffee_amount, muffin_amount)
+
+#Final Result
+print("\nTotal Cost: $", total_cost)
