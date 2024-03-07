24bf51b1ddfb57ab6f6a14c94f8323fbb4355825
dalvarez59 <160683173+dalvarez59@users.noreply.github.com>
2024-03-06 23:35:37 -0700

    Update cis129_lab03_coffeeShop.py

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -1,13 +1,15 @@
 print("My Coffee and Muffin Shop Receipt")
 
-def calculate_total_cost(coffee_amount, muffin_amount):
+def calculate_total_cost(coffee_amount, muffin_amount, boba_amount, tea_amount):
     #Prices
     coffee_price = 5
     muffin_price = 4
+    boba_price = 4
+    tea_price = 3
     tax_rate = .06
     
     #Subtotal
-    subtotal = (coffee_amount * coffee_price) + (muffin_amount * muffin_price)
+    subtotal = (coffee_amount * coffee_price) + (muffin_amount * muffin_price) + (boba_amount * boba_price) + (tea_amount * tea_price)
     
     #Taxes
     tax_amount = subtotal * tax_rate
@@ -20,9 +22,11 @@ def calculate_total_cost(coffee_amount, muffin_amount):
 #Customer's order
 coffee_amount = int(input("Amount of Coffees: "))
 muffin_amount = int(input("Amount of Muffins: "))
+boba_amount = int(input("Amount of Boba: "))
+tea_amount = int(input("Amount of Tea: "))
 
 #Calculate total cost 
-total_cost = calculate_total_cost(coffee_amount, muffin_amount)
+total_cost = calculate_total_cost(coffee_amount, muffin_amount, boba_amount, tea_amount)
 
 #Final Result
-print("\nTotal Cost: $", total_cost)
+print("\nTotal Cost: $", total_cost, "\n\nThank you for your purchase!")
